54241ef7f1634667f265b6ca7bdc1193b5be8967
badjilounes <lounes@silk-digital.com>
2018-05-23 09:51:37 +0200

    delete unwanted files

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './src/image-zoom.directive';
-export * from './src/image-zoom-lens.component';
-export * from './src/image-zoom-container.component';
-export declare class ImageZoomModule {
-}
